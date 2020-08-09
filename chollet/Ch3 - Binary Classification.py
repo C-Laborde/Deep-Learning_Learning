504bde6023c2db9947aecd6d6f7c001654f97229
Cecilia Laborde <cecilia@patchwork.health>
2020-08-09 14:36:24 +0200

    Enconding inputs

--- chollet/Ch3 - Binary Classification.py
+++ chollet/Ch3 - Binary Classification.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -17,6 +18,7 @@ from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = "all"
 
 from keras.datasets import imdb
+import numpy as np
 # -
 
 # Only the top 10000 most frequently occurring words in the training date are kept
@@ -26,12 +28,39 @@ train_data.shape
 test_data.shape
 
 # Decoding back to English
+review_nr = 1
 word_index = imdb.get_word_index()
 reverse_word_index = dict(
     [(value, key) for (key, value) in word_index.items()]
 )
-decoded_review =' '.join(reverse_word_index.get(i-3, '?') for i in train_data[0])
+decoded_review =' '.join(reverse_word_index.get(i-3, '?') for i in train_data[review_nr])
 
 decoded_review
 
 
+# ### Encoding the integer sequences into a binary matrix
+
+# (From Chollet's book)
+# There are two ways to do that:
+# - Pad your lists so that they all have the same length, turn them into an integer tensor of shape (samples, word_indices), and then use as the first layer in your network a layer capable of handling such integer tensors (the Embedding layer, which we’ll cover in detail later in the book).
+# - One-hot encode your lists to turn them into vectors of 0s and 1s. This would mean, for instance, turning the sequence [3, 5] into a 10,000-dimensional vec- tor that would be all 0s except for indices 3 and 5, which would be 1s. Then you could use as the first layer in your network a Dense layer, capable of handling floating-point vector data.
+
+# Manual implementation for clarity
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+x_train[1]
+
+# Why do we transform into array if they are array already?
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+# + active=""
+#
