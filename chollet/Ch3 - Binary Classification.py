b17b45d5e37ce3864e2a7076b7e358430b6edc4a
Cecilia Laborde <cecilia@patchwork.health>
2020-08-16 15:01:36 +0200

    IMBD network architecture

--- chollet/Ch3 - Binary Classification.py
+++ chollet/Ch3 - Binary Classification.py
@@ -17,18 +17,27 @@
 from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = "all"
 
-from keras.datasets import imdb
 import numpy as np
+
+from keras import models
+from keras import layers
 # -
 
-# Only the top 10000 most frequently occurring words in the training date are kept
+from keras.datasets import imdb
+
+# Only the top 10000 most frequently occurring words in the training data are kept
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
 train_data.shape
 test_data.shape
 
-# Decoding back to English
+# + jupyter={"outputs_hidden": true}
+# Each sample is a list of words forming a review, encoded as a sequence of integers
 review_nr = 1
+train_data[1]
+# -
+
+# Decoding back to English
 word_index = imdb.get_word_index()
 reverse_word_index = dict(
     [(value, key) for (key, value) in word_index.items()]
@@ -40,12 +49,14 @@ decoded_review
 
 # ### Encoding the integer sequences into a binary matrix
 
-# (From Chollet's book)
+# (From Chollet's book) The training lists have to be turned into tensors.
 # There are two ways to do that:
 # - Pad your lists so that they all have the same length, turn them into an integer tensor of shape (samples, word_indices), and then use as the first layer in your network a layer capable of handling such integer tensors (the Embedding layer, which we’ll cover in detail later in the book).
-# - One-hot encode your lists to turn them into vectors of 0s and 1s. This would mean, for instance, turning the sequence [3, 5] into a 10,000-dimensional vec- tor that would be all 0s except for indices 3 and 5, which would be 1s. Then you could use as the first layer in your network a Dense layer, capable of handling floating-point vector data.
+# - One-hot encode your lists to turn them into vectors of 0s and 1s. This would mean, for instance, turning the sequence [3, 5] into a 10,000-dimensional vector that would be all 0s except for indices 3 and 5, which would be 1s. Then you could use as the first layer in your network a Dense layer, capable of handling floating-point vector data.
+#
+# **QUESTION: In the one-hot encoder, how do you indicate if a word shows up more than once?**
 
-# Manual implementation for clarity
+# Manual implementation of second way for clarity
 def vectorize_sequences(sequences, dimension=10000):
     results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
@@ -56,11 +67,46 @@ def vectorize_sequences(sequences, dimension=10000):
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
 
+len(x_train[1])
 x_train[1]
 
-# Why do we transform into array if they are array already?
+# Labels should be vectorized as well
+# QUESTION Why do we transform into array if they are array already?
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
 
-# + active=""
+# ### Network arquitecture ##
 #
+# The input data is vectors, and the labels are scalars (1s and 0s): this is the EASIEST setup you'll ever encounter. A type of vector that performs well on such problem i a simple stack of fully connected (Dense) layers with _relu_ activations.
+# **QUESTION: Why is this network suitable?** </br>
+# **QUESTION: How does the math work? Each input is a vector of size (10000) of 0 and 1, each encoded value is multiplied by the weighs?**
+
+# (S1, S2, ..., SN)     dot
+
+# | W11 | ... | W1-16 |
+# | --- | --- | --- |
+# | W21 | ... | W2-16 |
+# | ... | ... | ... |
+# | WN1 | ... | WN-16 |
+
+# (S1 * W1-1 + S2 * W2-1 + ... + SN * WN-1, </br>
+#  S1 * W1-2 + S2 * W2-2 + ... + SN * WN-2, </br>
+#  ..., </br>
+#  S1 * W1-16 + S2 * W2-16 + ... + SN * WN-16)
+
+# S1 * W1-1 = [1, 0 ,0 , 1, ...., 0] * W1-1
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation="relu", input_shape=(10000,)))
+model.add(layers.Dense(16, activation="relu"))
+model.add(layers.Dense(1, activation="sigmoid"))
+
+#
+
+# Activation function: This is a binary classification problem and the output of the network is a probability (that's why we end the network with a single-unit layer with a sigmoid activation), it's best to use the _binary_crossentropy_ loss"
+
+model.compile(optimizer="rmsprop",
+              loss="binary_crossentropy",
+              metrics=["accuracy"])
+
+
