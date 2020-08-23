516746df49cac3d2668cad314e7fbc53fae2a8f9
Cecilia Laborde <cecilia@patchwork.health>
2020-08-23 14:49:19 +0200

    More epochs and plotting

--- chollet/Ch3 - Regression.py
+++ chollet/Ch3 - Regression.py
@@ -15,6 +15,7 @@
 # ## Boston Housing Price Prediction
 
 # +
+import matplotlib.pyplot as plt
 import numpy as np
 
 from keras.datasets import boston_housing
@@ -102,3 +103,60 @@ for i in range(k):
 all_scores
 
 np.mean(all_scores)
+
+# We will try with longer epochs
+
+# +
+num_epochs = 500
+all_mae_histories = []
+
+for i in range(k):
+    print("Processing fold #", i)
+    val_data = train_data[i * num_val_samples : (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples : (i + 1) * num_val_samples]
+    
+    partial_train_data = np.concatenate(
+        [train_data[: i * num_val_samples],
+         train_data[(i+1) * num_val_samples :]],
+        axis=0
+    )
+    partial_train_targets = np.concatenate(
+        [train_targets[: i * num_val_samples],
+         train_targets[(i+1) * num_val_samples :]]
+    )
+    
+    model = build_model()
+    history = model.fit(partial_train_data, partial_train_targets,
+                        epochs = num_epochs, batch_size=1, verbose=0)
+    mae_history = history.history['mae']
+    all_mae_histories.append(mae_history)
+# -
+
+average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+
+plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
+plt.xlabel("Epochs")
+plt.ylabel("Validation MAE")
+
+
+# +
+# For better plotting we apply an exponential moving average to smooth the curve
+
+def smooth_curve(points, factor=0.9):
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smooted_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
+
+
+# -
+
+smoothed_mae_history = smooth_curve(average_mae_history[10:])
+
+plt.plot(range(1, len(smooth_mae_history) + 1), smooth_mae_history)
+plt.xlabel("Epochs")
+plt.ylabel("Validation MAE")
