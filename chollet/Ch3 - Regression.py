fa7d96ed8b00fd53e7995cd2d8cd0443f76f117a
Cecilia Laborde <cecilia@patchwork.health>
2020-08-23 14:36:34 +0200

    First training results

--- chollet/Ch3 - Regression.py
+++ chollet/Ch3 - Regression.py
@@ -84,12 +84,12 @@ for i in range(k):
     
     partial_train_data = np.concatenate(
         [train_data[: i * num_val_samples],
-         train_data[(i+1) * num_val_samples, :]],
+         train_data[(i+1) * num_val_samples :]],
         axis=0
     )
-    partail_train_targets = np.concatenate(
+    partial_train_targets = np.concatenate(
         [train_targets[: i * num_val_samples],
-         train_targets[(i+1) * num_val_samples, :]]
+         train_targets[(i+1) * num_val_samples :]]
     )
     
     model = build_model()
@@ -97,3 +97,8 @@ for i in range(k):
               epochs = num_epochs, batch_size=1, verbose=0)
 
     val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
+    all_scores.append(val_mae)
+
+all_scores
+
+np.mean(all_scores)
